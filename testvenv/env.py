dd4dc34e9a7c6e745e35de8138e2b2288da24c89
Chonlasitsk <t.chonlasits@gmail.com>
2023-11-10 22:56:26 +0700

    add function

--- testvenv/env.py
+++ testvenv/env.py
@@ -1,2 +1,6 @@
-a = input('Enter your name :')
-print('Welcome %s to my Repository!!' %a)
+name = input('Enter your name :')
+print('Welcome %s to my Repository!!' %name)
+if name == 'Tawan':
+    print('Welcome admin !!!')
+else:
+    print('Who are you ??')
